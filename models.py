471720421ba3264f1d27b6d15da3c5b64433867c
shidehrzf <shideh.rezaeifar@unige.ch>
2020-03-04 21:12:24 +0100

    added resnet

--- models.py
+++ models.py
@@ -9,10 +9,67 @@ import numpy as np
 # slightly modified for cifar10
 #
 # Resnet, Copy-paste from torchvision.models.resnet,
-class ResNet(nn.Module):
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        print('\n input of first convolution:{}'.format(x.size()))
+        out = F.relu(self.bn1(self.conv1(x)))
+        print('\n output of first convolution:{}'.format(out.size()))
+
+        out = self.bn2(self.conv2(out))
+        # print(out.size())
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNetC(nn.Module):
 
     def __init__(self, block, layers, num_classes=10):
-        super(ResNet, self).__init__()
+        super(ResNetC, self).__init__()
         self.in_planes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
@@ -48,12 +105,8 @@ class ResNet(nn.Module):
 
 
 def resnet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-
-
-def resnet18_attention():
-    return ResNet(AttentionBlock, [1, 1, 1, 1])
+    return ResNetC(BasicBlock, [2, 2, 2, 2])
 
 
 def resnet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
+    return ResNetC(Bottleneck, [3, 4, 6, 3])
