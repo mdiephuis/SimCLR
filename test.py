ea12ab9e6d3611a72f1697feecb8ac7875fb2bfe
Maurits Diephuis <mdiephuis@gmail.com>
2020-03-05 11:49:07 +0100

    test code

---
+++ test.py
@@ -0,0 +1,35 @@
+import argparse
+import torch
+from torch.optim import SGD
+from tensorboardX import SummaryWriter
+import os
+
+from models import *
+from utils import *
+from data import *
+
+
+# Enable CUDA, set tensor type and device
+use_cuda = False
+if use_cuda:
+    dtype = torch.cuda.FloatTensor
+    device = torch.device("cuda")
+    torch.cuda.set_device(0)
+    print('GPU')
+else:
+    dtype = torch.FloatTensor
+    device = torch.device("cpu")
+
+
+# Get train and test loaders for dataset
+in_channels = 3
+# Get train and test loaders for dataset
+loader = Loader('CIFAR10C', 'data', True, 32, None, None, use_cuda)
+train_loader = loader.train_loader
+test_loader = loader.test_loader
+
+model = resnet50()
+
+xi, xj, _ = next(iter(test_loader))
+
+hi = model(xi)
