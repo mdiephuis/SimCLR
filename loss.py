ed1471bc8973853224240db440e29f1de2e431d0
diephuis <Maurits.Diephuis@unige.ch>
2020-03-05 19:24:38 +0100

    deborked the loss

--- loss.py
+++ loss.py
@@ -3,30 +3,28 @@ import torch.nn as nn
 
 
 class contrastive_loss(nn.Module):
-    def __init__(self, tau=1, use_cuda):
+    def __init__(self, tau=1):
+        super(contrastive_loss, self).__init__()
         self.tau = tau
-        self.use_cuda = use_cuda
 
-    def forward(self, x):
-        b_sz = x.size(0) // 2
+    def forward(self, xi, xj):
+
+        x = torch.cat((xi, xj), dim=0)
 
         sim_mat_nom = torch.mm(x, x.T)
         sim_mat_denom = torch.mm(torch.norm(x, dim=1).unsqueeze(1), torch.norm(x, dim=1).unsqueeze(1).T)
-
         sim_mat = sim_mat_nom / sim_mat_denom.clamp(min=1e-16)
         sim_mat = torch.exp(sim_mat / self.tau)
 
         # getting rid of diag
-        diag_ind = torch.eye(b_sz * 2).bool()
+        diag_ind = torch.eye(xi.size(0) * 2).bool()
         sim_mat = sim_mat.masked_fill_(diag_ind, 0)
 
-        loss_nom = torch.zeros(b_sz).cuda() if self.use_cuda else torch.zeros(b_sz)
-        loss_denom = torch.zeros(b_sz).cuda() if self.use_cuda else torch.zeros(b_sz)
+        # top
+        sim_mat_denom = torch.norm(xi, dim=1) * torch.norm(xj, dim=1)
+        sim_match = torch.exp(torch.sum(xi * xj, dim=-1) / sim_mat_denom / self.tau)
+        sim_match = torch.cat((sim_match, sim_match), dim=0)
 
-        for i in range(b_sz):
-            loss_nom[i] = sim_mat[i][i + b_sz]
-            loss_denom[i] = torch.sum(sim_mat[i, :])
-
-        loss = torch.mean(-torch.log(loss_nom / loss_denom))
+        loss = torch.mean(-torch.log(sim_match / torch.sum(sim_mat, dim=-1)))
 
         return loss
