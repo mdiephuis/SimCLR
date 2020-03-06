9033d86f0cc1ae0fe7756203dd38d74b7be8ae66
diephuis <Maurits.Diephuis@unige.ch>
2020-03-06 16:29:07 +0100

    l2 normalizing now optional

--- loss.py
+++ loss.py
@@ -3,18 +3,21 @@ import torch.nn as nn
 
 
 class contrastive_loss(nn.Module):
-    def __init__(self, tau=1):
+    def __init__(self, tau=1, normalize=False):
         super(contrastive_loss, self).__init__()
         self.tau = tau
+        self.normalize = normalize
 
     def forward(self, xi, xj):
 
         x = torch.cat((xi, xj), dim=0)
 
         is_cuda = x.is_cuda
-        sim_mat_nom = torch.mm(x, x.T)
-        sim_mat_denom = torch.mm(torch.norm(x, dim=1).unsqueeze(1), torch.norm(x, dim=1).unsqueeze(1).T)
-        sim_mat = sim_mat_nom / sim_mat_denom.clamp(min=1e-16)
+        sim_mat = torch.mm(x, x.T)
+        if self.normalize:
+            sim_mat_denom = torch.mm(torch.norm(x, dim=1).unsqueeze(1), torch.norm(x, dim=1).unsqueeze(1).T)
+            sim_mat = sim_mat / sim_mat_denom.clamp(min=1e-16)
+
         sim_mat = torch.exp(sim_mat / self.tau)
 
         # no diag because it's not diffrentiable -> sum - exp(1 / tau)
@@ -24,8 +27,12 @@ class contrastive_loss(nn.Module):
         # sim_mat = sim_mat.masked_fill_(diag_ind, 0)
 
         # top
-        sim_mat_denom = torch.norm(xi, dim=1) * torch.norm(xj, dim=1)
-        sim_match = torch.exp(torch.sum(xi * xj, dim=-1) / sim_mat_denom / self.tau)
+        if self.normalize:
+            sim_mat_denom = torch.norm(xi, dim=1) * torch.norm(xj, dim=1)
+            sim_match = torch.exp(torch.sum(xi * xj, dim=-1) / sim_mat_denom / self.tau)
+        else:
+            sim_match = torch.exp(torch.sum(xi * xj, dim=-1) / self.tau)
+
         sim_match = torch.cat((sim_match, sim_match), dim=0)
 
         norm_sum = torch.exp(torch.ones(x.size(0)) / self.tau)
