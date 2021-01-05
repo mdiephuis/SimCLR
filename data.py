fed5007638707336596afd1290576eba95b1d8d1
Maurits Diephuis <mdiephuis@gmail.com>
2021-01-05 20:27:25 +0100

    removed normalization for cifar

--- data.py
+++ data.py
@@ -19,8 +19,7 @@ def cifar_train_transforms():
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
         transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        transforms.ToTensor()
     ])
     return all_transforms
 
@@ -31,8 +30,7 @@ def cifar_test_transforms():
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
         transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        transforms.ToTensor()
     ])
     return all_transforms
 
